43d54c64a322b13898e653aa0e9e0474f148a501
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-07-12 13:56:49 +0100

    Refactored: Removed core from LS for now

--- src/controls/ls.ts
+++ src/controls/ls.ts
@@ -1,14 +1,7 @@
-import * as fs from "fs";
-import { Core } from "../controls/core";
-import { IDependencies } from "../interfaces/idependencies";
-
 export class LS {
-
-    private _core = new Core();
-
     constructor() { }
 
     public async dependencies() {
-        return await this._core.packages();
+    //  to do
     }
 }
