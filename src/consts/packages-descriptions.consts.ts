ca055b51933d5816ad2e16b949f4d5e72f9e094b
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-07-12 10:45:22 +0100

    Feature: Package descriptions consts

---
+++ src/consts/packages-descriptions.consts.ts
@@ -0,0 +1,21 @@
+export class PackageDescriptionsConsts {
+    public static audit: string = "Audits the dependencies for any security";
+    public static build: string = "Builds the project";
+    public static init: string = "Create a ethereum.json file";
+    public static install: string = "Installs a package";
+    public static login: string = "Logs the user in to ethereum-pm";
+    public static logout: string = "Logs the user out of the ethereum-pm";
+    public static ls: string = "Gives a list of the installed packages";
+    public static outdated: string = "Tells the user which packages are outdated";
+    public static owner: string = "The owner"
+    public static ping: string = "Ping ethereumpm to make sure it is alive";
+    public static profile: string = "Shows the user basic profile details";
+    public static repo: string = "The repo details";
+    public static search: string = "Search a package on ethereumpm";
+    public static star: string = "Star a package (have to be logged in to do so)";
+    public static uninstall: string = "Uninstalls a package from project";
+    public static upgrade: string = "Upgrades a package";
+    public static upload: string = "Uploads a package";
+    public static version: string = "Updates the version of the package";
+    public static whoami: string = "Returns the current user logged into ethereum-pm"
+}
