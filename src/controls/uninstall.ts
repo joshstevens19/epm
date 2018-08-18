d77f8b999ac96cc43e0a8ace945d2b3eb634635c
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-08-19 00:48:03 +0100

    logic to uninstall a package

--- src/controls/uninstall.ts
+++ src/controls/uninstall.ts
@@ -1,3 +1,20 @@
+import { EthereumModules } from ".";
+import { EthereumPmJson } from "./ethereum-pm-json";
+
 export class Uninstall {
-    constructor() {}
+    constructor(
+        private _ethereumModules: EthereumModules,
+        private _ethereumPmJson: EthereumPmJson,
+    ) {}
+
+    /**
+     * Uninstalls a package
+     * - This removes it from the ethereum_modules
+     * - This removes it from the dependencies in ethereum-pm.json
+     * @param packageName The package name
+     */
+    public async uninstallPackage(packageName: string): Promise<void> {
+        this._ethereumModules.removeEthereumModuleFolder(packageName);
+        await this._ethereumPmJson.removeDependency(packageName);
+    }
 }
