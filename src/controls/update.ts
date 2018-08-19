49446652586a7baa8e72899784499f54d2f59d0d
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-08-19 12:10:17 +0100

    update logic written

--- src/controls/update.ts
+++ src/controls/update.ts
@@ -1,3 +1,27 @@
+import { Package } from ".";
+import { Install } from "./install";
+import { EthereumPmJson } from "./ethereum-pm-json";
+
 export class Update {
-    constructor() {}
+    constructor(
+        private _packageControl: Package,
+        private _installControl: Install,
+        private _ethereumPmJsonControl: EthereumPmJson,
+    ) { }
+
+    /**
+     * Updates the package to its latest version
+     * @param packageName The package name 
+     */
+    public async updatePackage(packageName: string): Promise<void> {
+        const latestVersion: string = this._packageControl.getLatestVersionForPackage(packageName);
+
+        const installedDependencies = await this._ethereumPmJsonControl.getDependencies();
+
+        if (installedDependencies[packageName] !== latestVersion) {
+            await this._installControl.installPackage(`${packageName}@${latestVersion}`);
+        } else {
+            throw new Error(`Already have the lastest '${packageName}' package installed`);
+        }
+    }
 }
