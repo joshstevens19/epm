f70ed7ca293f60d88b6775b3b4f5d1f0df47266d
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-08-19 12:09:57 +0100

    Outdated logic written

--- src/controls/outdated.ts
+++ src/controls/outdated.ts
@@ -1,3 +1,31 @@
+import { EthereumPmJson, EthereumModules } from ".";
+import { Package } from "./package";
+import { IPackageNameAndVersion } from "../interfaces/ipackage-name-and-version";
+
 export class Outdated {
-    constructor() {}
+
+    constructor(
+        private _ethereumPmJsonControl: EthereumPmJson,
+        private _packageControl: Package,
+    ) {}
+
+    /**
+     * Returns a list of all the outdated packages which have been installed
+     */
+    public async checkForOutdatedPackages(): Promise<IPackageNameAndVersion[]> {
+        const dependencies = await this._ethereumPmJsonControl.getDependencies();
+
+        const outdatedPackages: IPackageNameAndVersion[] = [];
+
+        for (const dependency in dependencies) {
+            const latestVersion = this._packageControl.getLatestVersionForPackage(dependency);
+            if (latestVersion !== dependencies[dependency])
+            outdatedPackages.push({
+                name: dependency,
+                version: latestVersion
+            })
+        } 
+
+        return outdatedPackages;
+    }
 }
