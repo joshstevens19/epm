a79c6a951aa0f6d3774b5f9ae79366691aed36ea
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-08-20 21:01:01 +0100

    ping control

--- src/controls/ping.ts
+++ src/controls/ping.ts
@@ -1,3 +1,15 @@
+import * as rp from "request-promise";
+import { PingApi } from "../api-wrappers/ping.api";
+
 export class Ping {
-    constructor() {}
+    constructor(
+        private _pingApi: PingApi
+    ) { }
+
+    /**
+     * Checks if the server is alive
+     */
+    public async alive(): Promise<boolean> {
+        return await this._pingApi.alive();
+    }
 }
