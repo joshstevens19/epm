9326490159e5d863e77b11005815c1b38dc0ca29
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-08-20 21:34:16 +0100

    extend initialise to have package + version context

--- src/common/initialise-apis.ts
+++ src/common/initialise-apis.ts
@@ -1,7 +1,11 @@
 import { PingApi } from "../api-wrappers/ping.api";
+import { PackageApi } from "../api-wrappers/package.api";
+import { VersionApi } from "../api-wrappers/version.api";
 
 export class InitialiseApis {
     private static _pingApi: PingApi;
+    private static _packageApi: PackageApi;
+    private static _versionApi: VersionApi;
 
     public static get pingApi(): PingApi {
         if (InitialiseApis._pingApi) {
@@ -9,5 +13,21 @@ export class InitialiseApis {
         }
 
         return InitialiseApis._pingApi = new PingApi();
+    }
+
+    public static get packageApi(): PackageApi {
+        if (InitialiseApis._packageApi) {
+            return InitialiseApis._packageApi;
+        }
+
+        return InitialiseApis._packageApi = new PackageApi();
+    }
+
+    public static get versionApi(): VersionApi {
+        if (InitialiseApis._versionApi) {
+            return InitialiseApis._versionApi;
+        }
+
+        return InitialiseApis._versionApi = new VersionApi();
     }
 }
