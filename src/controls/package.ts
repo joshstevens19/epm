464348c3441317d1671cd25f94686e36dd6235b6
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-08-20 21:34:57 +0100

    tiny package control up to contain no API logic only wrappers

--- src/controls/package.ts
+++ src/controls/package.ts
@@ -1,22 +1,14 @@
-import * as rp from "request-promise";
 import { IPackageNameAndVersion } from "../interfaces/ipackage-name-and-version";
 import { IPackageFile } from "../interfaces/ipackage-file";
+import { PackageApi } from "../api-wrappers/package.api";
+import { VersionApi } from "../api-wrappers/version.api";
 
-// CURRENTLY DONE FOR DEV REASON, WILL BE MOVING OVER TO A API WHICH HOLDS 
-// ALL THE INFO ABOUT THE PACKAGES
 export class Package {
 
-    // will go into a config eventually at the moment it is 
-    // the port i have running for epm-api
-    // MAYBE MOVE THESE ENDPOINTS!! - MAYBE SOME STATIC BUILDERS TO BUILD THEM UP ;)
-    public static API_ROOT = "http://localhost:49936/api";
-    public static API_VERSIONS = '/versions';
-    public static API_PACKAGES = '/packages';
-
-    public static API_VERSIONS_ENDPOINT = Package.API_ROOT + Package.API_VERSIONS;
-    public static API_PACKAGES_ENDPOINT = Package.API_ROOT + Package.API_PACKAGES;
-
-    constructor() { }
+    constructor(
+        private _packageApi: PackageApi,
+        private _versionApi: VersionApi,
+    ) { }
 
     /**
      * Maybe needs renaming - splits packageName into a IPackageNameAndVersion
@@ -42,8 +34,7 @@ export class Package {
      * @param packageName The package name
      */
     public async getLatestVersionForPackage(packageName: string): Promise<string> {
-        const version = JSON.parse(await rp.get(`${Package.API_VERSIONS_ENDPOINT}/${packageName}/latest`));
-        return version.latestVersion;
+        return await this._versionApi.getLatestVersionForPackage(packageName);
     }
 
     /**
@@ -51,11 +42,6 @@ export class Package {
      * @param packageNameAndVersion The package name and version
      */
     public async getPackageFiles(packageNameAndVersion: IPackageNameAndVersion): Promise<IPackageFile[]> {
-        if (!packageNameAndVersion.version) {
-            throw new Error("you must supply a version");
-        }
-
-        const packageFiles = JSON.parse(await rp.get(`${Package.API_PACKAGES_ENDPOINT}/${packageNameAndVersion.name}/latest`)) as IPackageFile[];
-        return packageFiles;
+        return await this._packageApi.packageFiles(packageNameAndVersion);
     }
 }
