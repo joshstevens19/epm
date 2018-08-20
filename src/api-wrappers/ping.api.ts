ee7d7c8be32560fde892b6120d43b658cd109be0
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-08-20 20:28:17 +0100

    ping api wrapper

---
+++ src/api-wrappers/ping.api.ts
@@ -0,0 +1,19 @@
+import * as rp from "request-promise";
+import { CommonPropertiesApi } from "./common.api";
+
+export class PingApi {
+    public static ENDPOINT = "/Ping";
+
+    /**
+     * Sees if epm server is alive
+     */
+    public async alive(): Promise<boolean> {
+        try {
+            await rp.get(CommonPropertiesApi.buildApiUrlEndpoint(PingApi.ENDPOINT));
+        } catch(error) {
+            return false;
+        }
+        
+        return true;
+    }
+}
