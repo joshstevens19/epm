3aa9dca4b22bb8304a6a20e7432e8754253b205f
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-08-20 21:33:58 +0100

    creation of version.api wrapper

---
+++ src/api-wrappers/version.api.ts
@@ -0,0 +1,26 @@
+import * as rp from "request-promise";
+import { CommonPropertiesApi } from "./common.api";
+
+export class VersionApi {
+    
+    public static ENDPOINT = "/versions";
+    public static LATEST_VERSION_ENDPOINT = "/latest";
+
+    /**
+     * Gets the latest version for a package
+     * @param packageName The package name
+     */
+    public async getLatestVersionForPackage(packageName: string): Promise<string> {
+        const version = JSON.parse(await rp.get(this.latestVersionPackageEndPoint(packageName)));
+        return version.latestVersion;
+    }
+
+    /**
+     * Build the latest version package endpoint
+     * @param packageName The package name
+     */
+    private latestVersionPackageEndPoint(packageName: string): string {
+        const endpointPath: string = `${VersionApi.ENDPOINT}/${packageName}/${VersionApi.LATEST_VERSION_ENDPOINT}`
+        return CommonPropertiesApi.buildApiUrlEndpoint(endpointPath)
+    }
+}
