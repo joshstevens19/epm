b974e4bc69a58eb8794f111b73ec7d5b06174c7c
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-08-20 21:33:50 +0100

    creation of package.api wrapper

---
+++ src/api-wrappers/package.api.ts
@@ -0,0 +1,40 @@
+import * as rp from "request-promise";
+import { IPackageFile } from "../interfaces/ipackage-file";
+import { IPackageNameAndVersion } from "../interfaces/ipackage-name-and-version";
+import { CommonPropertiesApi } from "./common.api";
+
+export class PackageApi {
+    public static ENDPOINT = "/packages";
+    public static LATEST_VERSION_ENDPOINT = "/latest";
+
+    /**
+     * Gets the package files from the package name and version passed in
+     * @param packageNameAndVersion The package name and version
+     */
+    public async packageFiles(packageNameAndVersion: IPackageNameAndVersion): Promise<IPackageFile[]> {
+        if (!packageNameAndVersion.version) {
+            throw new Error("you must supply a version");
+        }
+        // need to start using version for API calls as well as latest
+        const packageFiles = JSON.parse(await rp.get(this.latestPackageEndPoint(packageNameAndVersion.name))) as IPackageFile[];
+        return packageFiles;
+    }
+
+    /**
+     * Builds the latest package install API call
+     * @param packageName The package name
+     */
+    private latestPackageEndPoint(packageName: string): string {
+        const endpointPath: string = `${PackageApi.ENDPOINT}/${packageName}/${PackageApi.LATEST_VERSION_ENDPOINT}`
+        return CommonPropertiesApi.buildApiUrlEndpoint(endpointPath)
+    }
+
+    /**
+     * Builds the version package install API call
+     * @param packageNameAndVersion The package and version
+     */
+    private versionPackageEndPoint(packageNameAndVersion: IPackageNameAndVersion): string {
+        const endpointPath: string = `${PackageApi.ENDPOINT}/${packageNameAndVersion.name}/${packageNameAndVersion.version}`;
+        return CommonPropertiesApi.buildApiUrlEndpoint(endpointPath);
+    }
+}
