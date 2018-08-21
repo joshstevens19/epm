1e301d34ae222d1b0fc781b23f73a7053b1bc46b
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-08-21 07:42:25 +0100

    Wrote basic logic for logout - work needed still

--- src/controls/logout.ts
+++ src/controls/logout.ts
@@ -1,8 +1,20 @@
+import { AuthenticationApi } from "../api-wrappers";
+
 export class Logout {
 
-    constructor() {}
+    constructor(
+        private _authenticationApi: AuthenticationApi
+    ) {}
 
-    public closeSession() {
-
+    /**
+     * unauthenticates the users session
+     * this means any private packages or commands which
+     * you need to be logged in for will now ask for
+     * authentication
+     */
+    public unauth(): void {
+        // need to clear the token stored on the users profile
+        // not sure if we should invalidate the token we created 
+        // for them as it is short living anyway?!
     }
 }
