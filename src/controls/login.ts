e13894b4d628b16b9c7575c5d80561105bd8ba83
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-08-21 07:35:33 +0100

    wrote login control layer

--- src/controls/login.ts
+++ src/controls/login.ts
@@ -1,3 +1,16 @@
+import { AuthenticationApi } from "../api-wrappers";
+
 export class Login {
-    constructor() {}
+    constructor(
+        private _authenticationApi: AuthenticationApi
+    ) {}
+
+    /**
+     * Authenticates the user
+     * @param username The username
+     * @param password The password
+     */
+    public async auth(username: string, password: string): Promise<void> {
+        await this._authenticationApi.login(username, password);
+    }
 }
