2b9a357d884dbfdb79679d7cd63a9a0fa4abe6a2
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-08-22 22:17:42 +0100

    added command controls to static properties

--- src/consts/controls-commands.ts
+++ src/consts/controls-commands.ts
@@ -2,4 +2,7 @@ export class ControlsCommands {
     public static readonly init: string = "init";
     public static readonly install: string = "install";
     public static readonly ls: string = "ls";
+    public static readonly outdated: string = "outdated";
+    public static readonly ping: string = "ping";
+    public static readonly login: string = "login";
 }
