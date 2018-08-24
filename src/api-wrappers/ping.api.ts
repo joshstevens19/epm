27401a675d78771756b2aec17b460f9744cca7b5
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-08-24 19:24:32 +0100

    convert ping api to use new http-request

--- src/api-wrappers/ping.api.ts
+++ src/api-wrappers/ping.api.ts
@@ -1,15 +1,20 @@
 import * as rp from "request-promise";
 import { CommonApi } from "./common.api";
+import { HttpRequest } from "./http-request";
 
 export class PingApi {
     public static ENDPOINT = "/ping";
+
+    constructor(
+        private _httpRequest: HttpRequest
+    ) { }
 
     /**
      * Sees if epm server is alive
      */
     public async alive(): Promise<boolean> {
         try {
-            await rp.get(CommonApi.buildApiUrlEndpoint(PingApi.ENDPOINT));
+            await this._httpRequest.get<boolean>(CommonApi.buildApiUrlEndpoint(PingApi.ENDPOINT));
         } catch(error) {
             return false;
         }
