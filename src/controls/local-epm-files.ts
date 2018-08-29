36119d3ad54fdc4dc353626fdc1b330611ef5dc5
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-08-29 19:43:47 +0100

    Started local-epm-files logic

---
+++ src/controls/local-epm-files.ts
@@ -0,0 +1,46 @@
+import * as fs from "fs-extra";
+import { Locations } from "../common/locations";
+
+export class LocalEpmFiles {
+
+    public async saveAuthenticationToken(jwtToken: string): Promise<void> {
+
+        try {
+            // create epm authenticaton folder if it does not exist
+            this.createEpmLocalAuthenticationFolder();
+
+            const authenticationBody = {
+                jwtToken,
+                createdOn: new Date(),
+            }
+
+            await fs.writeFile(Locations.epmUserHomeAuthenticationFileLocation, JSON.stringify(authenticationBody, null, 4));
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    /**
+     * Creates the epm local folder (if it does not already exist)
+     */
+    private createEpmLocalFolder(): void {
+        const epmLocation = Locations.epmUserHomeDir;
+        if (!fs.existsSync(epmLocation)) {
+            fs.mkdirSync(epmLocation);
+        }
+    }
+
+    /**
+     * Creates the epm local authentication folder (if it does not already exist)
+     */
+    private createEpmLocalAuthenticationFolder(): void {
+        this.createEpmLocalFolder();
+        const epmAuthenticationFolder = Locations.epmUserHomeDirAuthenticationLocation;
+        if (!fs.existsSync(epmAuthenticationFolder)) {
+            fs.mkdirpSync(epmAuthenticationFolder);
+        }
+    }
+}
+
+const test = new LocalEpmFiles();
+test.saveAuthenticationToken("TESTFAKETOKEN");
