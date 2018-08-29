1ed16183ded866500b8d5b43c17e870bb9009913
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-08-29 19:43:13 +0100

    jwtLocalFileName added to GenericConsts

--- src/consts/generic.consts.ts
+++ src/consts/generic.consts.ts
@@ -2,4 +2,5 @@ export class GenericConsts {
     public static readonly epmJsonName: string = "ethereum-pm.json";
     public static readonly epmModulesFolderName: string = "ethereum_modules";
     public static readonly root: string = "./"
+    public static readonly jwtLocalFileName: string = "jwt.json";
 }
