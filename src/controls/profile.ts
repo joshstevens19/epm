d853521ed59960033bb7ed52bc393236d8522e95
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-08-31 18:15:18 +0100

    mocked profile control

--- src/controls/profile.ts
+++ src/controls/profile.ts
@@ -1,3 +1,15 @@
 export class Profile {
     constructor() { }
+
+    public details() {
+        // read from JWT token as we can push all the data in their! 
+    }
+
+    public updateDetails() {
+        
+    }
+
+    public deleteProfile() {
+        
+    }
 }
