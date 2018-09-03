532029a4c03d1f8dd642c95d2422c873e065392e
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-09-03 17:50:29 +0100

    Added main logic to register

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import * as program from "commander"
 import { PackageDescriptionsConsts } from "./consts/packages-descriptions.consts";
 import * as chalk from "chalk";
 import { InitialiseControls } from "./common/initialise-controls";
+import { IRegister } from "./interfaces/iregister";
 
 // find TS library as we want the entire library to be in TS
 const ProgressBar = require('progress');
@@ -112,19 +113,53 @@ program
 program
   .command("login")
   .description(PackageDescriptionsConsts.login)
-  .option('--username <username>', 'The user to authenticate as')
+  .option('--username <username>', 'The username to authenticate')
   .option('--password <password>', 'The user\'s password')
   .action((dir, cmd) => {
     const username = dir.username;
     const password = dir.password;
 
-    if(username || password) {
+    if(username && password) {
       InitialiseControls.loginControl.authenticate(username, password)
                            .catch(err => console.log(err));
     } else {
       console.log("please supply and username or password")
     }
   });
+
+program
+  .command("register")
+  .description(PackageDescriptionsConsts.register)
+  .option('--username <username>', 'The username to sign in with')
+  .option('--password <password>', 'The users password')
+  .option('--firstName <firstName>', 'The users first name')
+  .option('--lastName <lastName>', 'The users lastname')
+  .option('--introduction <introudction>', 'The users introduction to show on the profile')
+  .action((dir, cmd) => {
+    const username = dir.username;
+    const password = dir.password;
+    const firstName = dir.firstName;
+    const lastName = dir.lastName;
+    const introduction = dir.introduction || null;
+    
+    if (username && password && firstName && lastName) {
+      const user: IRegister = {
+        emailAddress: username,
+        password,
+        firstName,
+        lastName,
+        introduction
+      };
+
+      InitialiseControls.registerControl.createUser(user)
+                                        .then(() => console.log("User registered"))
+                                        .catch((error: any) => console.error(error))
+
+    } else {
+      console.log("Please supply a username, password, firstname and lastname");
+    }
+
+  })
 
 program
     .command("logout")
