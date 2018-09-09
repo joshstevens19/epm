93c142149c0004ab08826d577df143642a11862e
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-09-09 14:36:56 +0100

    profile API logic

--- src/api-wrappers/profile.api.ts
+++ src/api-wrappers/profile.api.ts
@@ -1,25 +1,32 @@
 import * as rp from "request-promise";
 import { IProfile } from "../interfaces/iprofile";
 import { CommonApi } from "./common.api";
+import { HttpRequest } from ".";
+import { IUpdateProfileDetailsRequest } from "../interfaces/api-requests/iupdate-profile-details.request";
+import { IUpdateProfileDetailsResponse } from "../interfaces/api-reponses/iupdate-profile-details.response";
 
 export class ProfileApi {
     private static readonly ENDPOINT = "/profile";
+
+    constructor(
+        private _httpRequest: HttpRequest,
+    ) { }
     
     /**
      * The details for the logged in users profile
-     * @param jwtToken The JWT authentication token
      */
     public async details(): Promise<IProfile> {
         const endpoint: string = CommonApi.buildApiUrlEndpoint(ProfileApi.ENDPOINT);
-        return JSON.parse(await rp.get(endpoint)) as IProfile;
+        return await this._httpRequest.get<IProfile>(endpoint);
     }
 
     /**
      * Updating the profile details for the logged in user
      * @param profile The profile details to update too
-     * @param jwtToken The JWT authentication token
      */
-    public async updateDetails(profile: IProfile): Promise<void> {
-        // to do
+    public async updateDetails(profile: IUpdateProfileDetailsRequest): Promise<IUpdateProfileDetailsResponse> {
+        const endpoint: string = CommonApi.buildApiUrlEndpoint(ProfileApi.ENDPOINT);
+        
+        return await this._httpRequest.put<IUpdateProfileDetailsResponse>(endpoint, profile);
     }
 }
