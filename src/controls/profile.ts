9169d131deb0959449cab2af0f9d6bc8bcfbe91a
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-09-09 14:37:36 +0100

    update profile details, see your profile details

--- src/controls/profile.ts
+++ src/controls/profile.ts
@@ -1,15 +1,33 @@
-export class Profile {
-    constructor() { }
+import { ProfileApi } from "../api-wrappers";
+import { IProfile } from "../interfaces/iprofile";
+import { IUpdateProfileDetailsRequest } from "../interfaces/api-requests/iupdate-profile-details.request";
+import { LocalEpmFiles } from "./local-epm-files";
+import { IUpdateProfileDetailsResponse } from "../interfaces/api-reponses/iupdate-profile-details.response";
 
-    public details() {
-        // read from JWT token as we can push all the data in their! 
+export class Profile {
+    constructor(
+        private _profileApi: ProfileApi,
+        private _localEpmFilesControl: LocalEpmFiles,
+    ) { }
+
+    /**
+     * Gets the users profile details
+     */
+    public async details(): Promise<IProfile> {
+        return this._profileApi.details();
     }
 
-    public updateDetails() {
-        
+    /**
+     * Updates a users profile details
+     * @param profile the new profile details
+     */
+    public async updateDetails(profile: IUpdateProfileDetailsRequest): Promise<void> {
+        const response: IUpdateProfileDetailsResponse = await this._profileApi.updateDetails(profile);
+        await this._localEpmFilesControl.saveAuthenticationToken(response.jwtToken);
     }
 
     public deleteProfile() {
-        
+        // think about how this will work
+        // npm make you email support, feels wrong to do that
     }
 }
