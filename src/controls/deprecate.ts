1e0e8d4f0dfed56f193c6dbc9959194267add26c
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-09-12 19:14:22 +0100

    deprecated control

--- src/controls/deprecate.ts
+++ src/controls/deprecate.ts
@@ -1,3 +1,23 @@
+import { PackageApi } from "../api-wrappers";
+
 export class Deprecate {
+    
+    constructor(
+        private _packageApi: PackageApi,
+    ) { }
+
+    /**
+     * Deprecate a package 
+     */
+    public async deprecatePackage(packageName: string): Promise<void> {
+        await this._packageApi.deprecatePackage(packageName);
+    }
+
+    /**
+     * Undeprecates a package 
+     */
+    public async undeprecatePackage(packageName: string): Promise<void> {
+        await this._packageApi.undeprecatePackage(packageName);
+    }
     
 }
