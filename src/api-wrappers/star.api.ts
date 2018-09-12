10df0a153a0f7d3d13c949f631b4c6c792e33260
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-09-12 19:13:43 +0100

    star api logic

--- src/api-wrappers/star.api.ts
+++ src/api-wrappers/star.api.ts
@@ -1,8 +1,9 @@
 import { CommonApi } from "./common.api";
 import { HttpRequest } from "./http-request";
+import { IStarredPackageResponse } from "../interfaces/api-reponses/istarred-packages.response";
 export class StarApi {
  
-    private static readonly ENDPOINT = "/star";
+    private static readonly ENDPOINT = "/stars";
 
     constructor(
         private _httpRequest: HttpRequest
@@ -13,20 +14,42 @@ export class StarApi {
      * @param packageName The package name
      */
     public async starPackage(packageName: string): Promise<void> {
-        const uri = this.buildStarProjectEndPoint(packageName);
+        const uri = this.buildDefaultStarEndPoint();
         const body = {
-            package: packageName
+            packageName
         }
         
         await this._httpRequest.postVoid(uri, body);
     }
 
     /**
+     * Unstars the package for the user
+     * @param packageName The package name
+     */
+    public async unstarPackage(packageName: string): Promise<void> {
+        const uri = this.buildDefaultStarEndPoint();
+        const body = {
+            packageName
+        }
+        
+        await this._httpRequest.deleteVoid(uri, body); 
+    }
+
+    /**
+     * Gets all the starred projects for the logged in user
+     */
+    public async getAllStars(): Promise<IStarredPackageResponse> {
+        const uri = this.buildDefaultStarEndPoint();
+
+        return await this._httpRequest.get<IStarredPackageResponse>(uri);
+    }
+
+    /**
      * This builds the star project API endpoint
      * @param packageName The package name 
      */
-    private buildStarProjectEndPoint(packageName: string): string {
-        const endpointPath: string = `${StarApi.ENDPOINT}/${packageName}`;
+    private buildDefaultStarEndPoint(): string {
+        const endpointPath: string = `${StarApi.ENDPOINT}`;
         return CommonApi.buildApiUrlEndpoint(endpointPath);
     }
 }
