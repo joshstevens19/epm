3c319c08a0ceee029c39ec57857a02bf8dbcc1de
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-09-12 19:15:06 +0100

    wrote star control logic

--- src/controls/star.ts
+++ src/controls/star.ts
@@ -1,3 +1,31 @@
+import { StarApi } from "../api-wrappers";
+import { IStarredPackageResponse } from "../interfaces/api-reponses/istarred-packages.response";
+
 export class Star {
-    constructor() {}
+    constructor(
+        private _starApi: StarApi
+    ) { }
+
+    /**
+     * Stars a package
+     * @param packageName The package name
+     */
+    public async starPackage(packageName: string): Promise<void> {
+        await this._starApi.starPackage(packageName);
+    }
+
+    /**
+     * Unstars a package
+     * @param packageName The package name 
+     */
+    public async unstarPackage(packageName: string): Promise<void> {
+        await this._starApi.unstarPackage(packageName);
+    }
+
+    /**
+     * Gets all the starred packages 
+     */
+    public async getAllStars(): Promise<IStarredPackageResponse> {
+        return await this._starApi.getAllStars();
+    }
 }
