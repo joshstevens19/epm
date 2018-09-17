13db1ac01a40b75505e6611365a479f75057c356
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-09-17 17:54:02 +0100

    extended jwt control to read more details about the token

--- src/controls/jwt.ts
+++ src/controls/jwt.ts
@@ -2,6 +2,7 @@ import * as fs from "fs-extra";
 import { Locations } from "../common/locations";
 import { LocalEpmFiles } from "./local-epm-files";
 import { IJwtDetails } from "../interfaces/ijwt-details";
+import * as jwt_decode from "jwt-decode";
 
 export class Jwt {
     constructor(
@@ -13,9 +14,55 @@ export class Jwt {
      */
     public async getJwtToken(): Promise<string | null> {
         if (this._localEpmFiles.authenticationFileExists()) {
-            const jwtDetails: IJwtDetails = JSON.parse(await fs.readFile(Locations.epmUserHomeAuthenticationFileLocation, 'utf8')) as IJwtDetails;
+            const jwtDetails: IJwtDetails = await this.getJwtDetails();
 
             return jwtDetails.jwtToken;
+        }
+
+        return null;
+    }
+
+    /**
+     * Get the created on date for the JWT
+     */
+    public async getJwtCreatedOnDate(): Promise<Date | null> {
+        if (this._localEpmFiles.authenticationFileExists()) {
+            const jwtDetails: IJwtDetails = await this.getJwtDetails();
+
+            return jwtDetails.createdOn;
+        }
+
+        return null;
+    }
+
+    /**
+     * Gets the jwt details 
+     */
+    public async getJwtDetails(): Promise<IJwtDetails> {
+        return JSON.parse(await fs.readFile(Locations.epmUserHomeAuthenticationFileLocation, 'utf8')) as IJwtDetails;
+    }
+
+    /**
+     * Returns the decoded JWT token
+     * This was chosen to be a `any` for a reason
+     */
+    public async decodeJwt(): Promise<any> {
+        const jwtToken = await this.getJwtToken();
+        if (jwtToken !== null) {
+            return jwt_decode(jwtToken);
+        }
+
+        return null;
+    }
+
+    /**
+     * Get JWT token expiry date (it could be in the pass)
+     * This does not validate if the token is correct 
+     */
+    public async getJwtExpiryDate(): Promise<Date | null> {
+        const jwtUnpacked = await this.decodeJwt();
+        if (jwtUnpacked !== null) {
+            return new Date(jwtUnpacked.exp * 1e3);
         }
 
         return null;
@@ -25,7 +72,7 @@ export class Jwt {
      * Removes the authentication file from the users computer 
      */
     public async deleteJwtAuthentication(): Promise<void> {
-        if (this._localEpmFiles.authenticationFileExists()) { 
+        if (this._localEpmFiles.authenticationFileExists()) {
             fs.remove(Locations.epmUserHomeAuthenticationFileLocation);
         }
     }
