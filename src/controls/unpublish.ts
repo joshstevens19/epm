6c49afca43dc15fce9ba8f40d1b60d366d82f854
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-09-18 08:00:26 +0100

    Unpublish control

--- src/controls/unpublish.ts
+++ src/controls/unpublish.ts
@@ -1,3 +1,15 @@
+import { PackageApi } from "../api-wrappers";
+
 export class Unpublish {
-    
+    constructor(
+        private _packageApi: PackageApi
+    ) { }
+
+    /**
+     * Unpublished a package
+     * @param packageName The package name
+     */
+    public async main(packageName: string): Promise<void> {
+        await this._packageApi.unpublishPackage(packageName);
+    }
 }
