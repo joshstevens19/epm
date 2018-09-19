9258c6b57914dbef3a136514b8e144356d7ecfc4
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-09-19 08:08:38 +0100

    mock search api

--- src/api-wrappers/search.api.ts
+++ src/api-wrappers/search.api.ts
@@ -1,3 +1,13 @@
+import { HttpRequest } from "./http-request";
+
 export class SearchApi {
-    
+    constructor(
+        private _httpRequest: HttpRequest
+    ) { }
+
+    // this will need to be a interface once written the backend search api
+    // and decided on what the interface will return 
+    public async queryByProjectName(projectName: string): Promise<void> {
+
+    }
 }
