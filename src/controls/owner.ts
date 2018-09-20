b46d3a4b1f576fca3a2a13d954decbaa1c272036
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-09-20 17:22:27 +0100

    write owner logic

--- src/controls/owner.ts
+++ src/controls/owner.ts
@@ -1,3 +1,43 @@
+import { PackageApi } from "../api-wrappers";
+import { IAdminUsersResponse } from "../interfaces/api-reponses/iadmin-users-response";
+
 export class Owner {
-    constructor() {}
+    constructor(
+        private _packageApi: PackageApi,
+    ) { }
+
+    /**
+     * Transfer the owner of a package
+     * @param packageName 
+     * @param username 
+     */
+    public async transferOwnerOfPackage(packageName: string, username: string): Promise<void> {
+        return await this._packageApi.transferOwnerOfPackage(packageName, username);
+    }
+
+    /**
+     * Add admin user to package
+     * @param packageName 
+     */
+    public async addAdmin(packageName: string, username: string): Promise<void> {
+        return await this._packageApi.addAdminToPackage(packageName, username);
+    }
+
+    /**
+     * Removes admins permission
+     * @param packageName The package name
+     * @param username The username 
+     */
+    public async removeAdminPermission(packageName: string, username: string): Promise<void> {
+        return await this._packageApi.removeAdminPermission(packageName, username);
+    }
+
+    /**
+     * Gets all the admins for the package
+     * @param packageName The package name
+     * @param username The username 
+     */
+    public async getAdminUsers(packageName: string): Promise<IAdminUsersResponse> {
+        return await this._packageApi.getAdminUsers(packageName);
+    }
 }
