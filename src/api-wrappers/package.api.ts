a585488aa4847eebd060fd2abfe9c5340505d625
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-09-20 17:21:41 +0100

    owner commands in the package api proxy

--- src/api-wrappers/package.api.ts
+++ src/api-wrappers/package.api.ts
@@ -2,14 +2,17 @@ import { IPackageNameAndVersion } from "../interfaces/ipackage-name-and-version"
 import { CommonApi } from "./common.api";
 import { HttpRequest } from "./http-request";
 import { IPackageFiles } from "../interfaces/ipackage-files";
+import { IAdminUsersResponse } from "../interfaces/api-reponses/iadmin-users-response";
 
 export class PackageApi {
     private static ENDPOINT = "/packages";
     private static OWNER_ENDPOINT = "/owner"
     private static IS_OWNER_ENDPOINT = "/isowner";
-    private static ADD_ADMIN_ENDPOINT = "/admin";
+    private static ADMIN_ENDPOINT = "/admin";
     private static DEPRECATE_ENDPOINT = "/deprecate";
+    private static TRANSFER_ENDPOINT = "transfer";
     private static UNDEPRECATE_ENDPOINT = "/undeprecate";
+    private static USER_ENDPOINT = "user";
     private static UNPUBLISH_ENDPOINT = "/unpubish";
 
     constructor(
@@ -71,7 +74,71 @@ export class PackageApi {
      * @param username The username
      */
     public async addAdminToPackage(packageName: string, username: string): Promise<void> {
-        const endpoint: string = this.addAdminToPackageEndPoint;
+        const endpoint: string = this.adminPackageEndPoint;
+
+        const body = {
+            packageName,
+            username
+        }
+
+        return await this._httpRequest.postVoid(endpoint, body);
+    }
+
+    /**
+     * Removes admin permissions for a user
+     * @param packageName The package name
+     * @param username The username
+     */
+    public async removeAdminPermission(packageName: string, username: string): Promise<void> {
+        const endpoint: string = this.adminPackageEndPoint;
+
+        const body = {
+            packageName,
+            username
+        }
+
+        return await this._httpRequest.deleteVoid(endpoint, body);
+    }
+
+    /**
+     * Add a user to a package 
+     * @param packageName The package name
+     * @param username The username
+     */
+    public async addUserToPackage(packageName: string, username: string): Promise<void> {
+        const endpoint: string = this.userPackageEndpoint;
+
+        const body = {
+            packageName,
+            username
+        }
+
+        return await this._httpRequest.postVoid(endpoint, body);
+    }
+
+    /**
+     * Remove a user from a package
+     * @param packageName The package name
+     * @param username The username
+     */
+    public async removeUserFromPackage(packageName:string, username:string): Promise<void> {
+        const endpoint:string = this.userPackageEndpoint;
+
+        const body = {
+            packageName,
+            username
+        }
+
+        return await this._httpRequest.deleteVoid(endpoint, body);
+    }
+
+    /**
+     * Transfer owner of the package
+     * @param packageName The package name
+     * @param username The username
+     */
+    public async transferOwnerOfPackage(packageName: string, username: string): Promise<void> {
+        const endpoint: string = this.transferPackageEndpoint;
 
         const body = {
             packageName,
@@ -123,10 +190,36 @@ export class PackageApi {
     }
 
     /**
+     * Gets the admin users
+     * @param packageName The package name  
+     */
+    public async getAdminUsers(packageName: string): Promise<IAdminUsersResponse> {
+        const endpoint: string = this.buildGetAdminUsersForPackageEndpoint(packageName);
+
+        return await this._httpRequest.get<IAdminUsersResponse>(endpoint);
+    }
+
+    /**
      * Builds the unpublished endpoint
      */
     private get unpublishEndpoint(): string {
         const endpoint: string = `${PackageApi.ENDPOINT}${PackageApi.UNPUBLISH_ENDPOINT}`;
+        return CommonApi.buildApiUrlEndpoint(endpoint);
+    }
+
+    /**
+     * Builds up the user package endpoint
+     */
+    private get userPackageEndpoint(): string {
+        const endpoint: string = `${PackageApi.ENDPOINT}${PackageApi.USER_ENDPOINT}`;
+        return CommonApi.buildApiUrlEndpoint(endpoint);
+    }
+
+    /**
+     * Builds the transfer package endpoint
+     */
+    private get transferPackageEndpoint(): string {
+        const endpoint: string = `${PackageApi.ENDPOINT}${PackageApi.TRANSFER_ENDPOINT}`;
         return CommonApi.buildApiUrlEndpoint(endpoint);
     }
 
@@ -149,8 +242,17 @@ export class PackageApi {
     /**
      * Builds the add admin to package endpoint
      */
-    private get addAdminToPackageEndPoint(): string {
-        const endpoint: string = `${PackageApi.ENDPOINT}${PackageApi.ADD_ADMIN_ENDPOINT}`;
+    private get adminPackageEndPoint(): string {
+        const endpoint: string = `${PackageApi.ENDPOINT}${PackageApi.ADMIN_ENDPOINT}`;
+        return CommonApi.buildApiUrlEndpoint(endpoint);
+    }
+
+    /**
+     * Build the get admin user endpoint
+     * @param packageName 
+     */
+    private buildGetAdminUsersForPackageEndpoint(packageName: string): string {
+        const endpoint: string = `${PackageApi.ENDPOINT}${packageName}`;
         return CommonApi.buildApiUrlEndpoint(endpoint);
     }
 
